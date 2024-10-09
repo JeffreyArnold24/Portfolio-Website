d6fbd1157ae987853198000f1921c1deb5187d34
Jeffrey Arnold <jeffarnold02@gmail.com>
2024-10-09 12:55:32 -0600

    Updates the comments for the LoginController and LoginService classes to contain java docs.

--- springBootServer/src/main/java/com/Controllers/LoginController.java
+++ springBootServer/src/main/java/com/Controllers/LoginController.java
@@ -13,12 +13,19 @@ import com.Services.LoginService;
 @RestController
 public class LoginController {
 
+    // Constructor for dependency injection.
     private LoginService loginService;
-
     public LoginController (LoginService loginService) {
         this.loginService = loginService;
     }
     
+    /** Takes a username and password from the user and either
+     *  creates them in the database or logs them in if the
+     *  credentials are correct.
+     * 
+     * @param request A class that contains a username and password
+     * @return        The username and authToken of the user if the sign in is successful.
+     */
     @PostMapping("/login")
     public LoginResponse sayHello(@RequestBody LoginRequest request) {
         LoginResponse response = loginService.login(request);
--- springBootServer/src/main/java/com/Services/LoginService.java
+++ springBootServer/src/main/java/com/Services/LoginService.java
@@ -30,7 +30,12 @@ public class LoginService{
         authTokenDAO = databaseConfig.getAuthTokenDAO();
     }
 
-    // Used to verify a valid request
+    /** Throws an error if either the username or password entered
+     *  by the user is empty.
+     * 
+     * @param request   The username and password entered by the user.
+     * @throws          Throws a bad_request error if their is either no username or password.
+     */
     private void verifyLoginCredentials(LoginRequest request){
         if (request.getUsername() == ""){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please include a Username.");
@@ -40,14 +45,33 @@ public class LoginService{
         }
     }
 
-    private String hashPassword(String Password){
-        return PasswordUtil.hashPassword(Password);
+    /** Used to take a password and hash it.
+     * 
+     * @param password      The password entered by the user.
+     * @return              The hashed version of the given password.
+     */
+    private String hashPassword(String password){
+        return PasswordUtil.hashPassword(password);
     }
 
+    /** Used to verify if the password is the same as
+     *  the hashed password
+     * 
+     * @param password          The password entered by the user.
+     * @param hashedPassword    The hashed password that corresponds to the user provided username.
+     * @return                  If the password and hashed password are the same.
+     */
     private Boolean verifyPasswordCredetials(String password, String hashedPassword){
         return PasswordUtil.verifyPassword(password, hashedPassword);
     }
 
+
+    /** Creates and assigns an authToken to the given user.
+     * 
+     * @param username  The user to create the authToken for.
+     * @return          The authToken created.
+     * @throws          Throws an internal server error if an authToken could not be created.
+     */
     private String createAuthToken(String username){
         
         String authToken = AuthTokenUtil.generateAuthToken();
@@ -60,8 +84,13 @@ public class LoginService{
         
     }
 
-    // Used to create a user in the database and assign them
-    // and Authtoken.
+    /** Used to create a user in the database and assign them
+     *  an authToken.
+     * 
+     * @param request   A class with the username and password of the user.
+     * @return          A class that contains the username and authToken of the user.
+     * @throws          Throws an internal server error if the user could not be created.
+     */
     public LoginResponse createUser(LoginRequest request){
         String hashedPassword = hashPassword(request.getPassword());
         if (usersDAO.createUser(request.getUsername(), hashedPassword)){
@@ -73,8 +102,13 @@ public class LoginService{
         }
     }
 
-    // Used to verify an existing username and password.
-    // Assigns an Authtoken on successful sign-in
+    /** Used to sign in a user if they exist in the database and
+     *  assign them an authToken.
+     * 
+     * @param request   A class with the username and password of the user.
+     * @return          A class that contains the username and authToken of the user.
+     * @throws          Throws an bad request error if the given password is incorrect.
+     */
     public LoginResponse signInUser(LoginRequest request){
         String retreivedPassword = usersDAO.getPassword(request.getUsername());
         if(verifyPasswordCredetials(request.getPassword(), retreivedPassword)){
@@ -86,7 +120,12 @@ public class LoginService{
         }
     }
 
-
+    /** Used to either sign in or create a user depending
+     *  on whether they exist in the database or not.
+     * 
+     * @param request   A class with the username and password of the user.
+     * @return          A class that contains the username and authToken of the user.
+     */
     public LoginResponse login(LoginRequest request){
         verifyLoginCredentials(request);
         LoginResponse response = null;
