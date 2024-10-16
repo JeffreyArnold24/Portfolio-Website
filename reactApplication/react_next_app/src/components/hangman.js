23abb5bd627884b4237434496dbfa7f1443dac28
Jeffrey Arnold <jeffarnold02@gmail.com>
2024-10-16 09:46:03 -0600

    Redoes some CSS to put it in the correct files. This fixes a bug where the top toolbar would not load the correct CSS if it didn't start on the main page.

--- reactApplication/react_next_app/src/components/hangman.js
+++ reactApplication/react_next_app/src/components/hangman.js
@@ -14,22 +14,23 @@ export default function Hangman() {
     return (
         <div className="main-container">
             <div className="hangman">
-                <h2>Hangman</h2>
-                {!hangmanGameStarted ? (
-                    <button onClick={() => start_game()}>Start Game</button>
-                ) : (<></>)}
-                <p>Guess the word:</p>
-                <p>Remaining attempts:</p>
-
-
-                <div>
-                <input
-                    type="text"
-                    maxLength="1"
-                    placeholder="Enter a letter"
-                />
+                <p>Hangman</p>
+                <div className="gameStart">
+                    <label htmlFor="numberDropdown">Number of letters: </label>
+                    <select id="numberDropdown">
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </select>
+                    {!hangmanGameStarted ? (
+                        <button onClick={() => start_game()}>Start Game</button>
+                    ) : (<button onClick={() => start_game()}>Reset Game</button>)}
                 </div>
-
 
             </div>
         </div>
