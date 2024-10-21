c627672e9ea14864d54901d647501fe36d7d97ab
Jeffrey Arnold <jeffarnold02@gmail.com>
2024-10-21 14:02:23 -0600

    Restructures the flask server to use app folders which will currently only contain the views.

---
+++ flaskServer/hangman/views.py
@@ -0,0 +1,11 @@
+from hangman import app
+from flask import jsonify, request
+
+items = [
+    {'id': 1, 'name': 'Item 1', 'price': 10.99},
+    {'id': 2, 'name': 'Item 2', 'price': 19.99}
+]
+
+@app.route('/hangman/starthangman', methods=['POST'])
+def get_items():
+    return jsonify(items), 200
