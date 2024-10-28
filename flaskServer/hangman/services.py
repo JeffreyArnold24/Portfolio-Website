284bff7bfcc054bcd14a874d902ef635176df2d3
Jeffrey Arnold <jeffarnold02@gmail.com>
2024-10-28 16:59:13 -0600

    Adds a method to determine if the user already has a game instance.

--- flaskServer/hangman/services.py
+++ flaskServer/hangman/services.py
@@ -6,20 +6,30 @@ from authentication.services import authTokenExists
 def create_word(numberCharacters):
     return '_' * numberCharacters
 
+def does_user_have_game(username):
+    game_instance = HangmanGameInstance.query.filter_by(username=username).first()
+    if game_instance:
+        return True
+    return False
+
+
 def initialize_game(username, authToken, numberCharacters):
 
     if not authTokenExists(authToken):
         return "Error"
     
-    new_word = create_word(numberCharacters)    
-    game_instance = HangmanGameInstance(
-        username = username,
-        guessedLetters = "",
-        usedGuesses = 0,
-        currentWord = new_word
-    )
+    if does_user_have_game(username):
+        return "True"
+    else:
+        new_word = create_word(numberCharacters)    
+        game_instance = HangmanGameInstance(
+            username = username,
+            guessedLetters = "",
+            usedGuesses = 0,
+            currentWord = new_word
+        )
 
-    db.session.add(game_instance)
-    db.session.commit()
+        db.session.add(game_instance)
+        db.session.commit()
 
     return new_word
