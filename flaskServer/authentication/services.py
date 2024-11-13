8d267c9a3a7aa81a0fdc956fa7acf1f38a90a348
Jeffrey Arnold <jeffarnold02@gmail.com>
2024-11-13 14:03:49 -0700

    Adds comments in the authToken service and hangman init file.

--- flaskServer/authentication/services.py
+++ flaskServer/authentication/services.py
@@ -1,7 +1,14 @@
 from .models import AuthToken
 from authentication import db
 
+"""
+Checks if an authToken exists in the database.
 
+:param authToken: The authToken to check for.
+
+:return: True if the authToken exists and false otherwise.
+
+"""
 def authTokenExists(authToken):
 
     auth_token_instance = AuthToken.query.filter_by(authToken=authToken).first()
--- flaskServer/hangman/__init__.py
+++ flaskServer/hangman/__init__.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///../../sqlite database/web_database.db'
 db = SQLAlchemy()
 db.init_app(app)
+
+# load the dictionary for hangman
 with open("resources/dictionary.txt", "r") as file:
     wordList = file.read().splitlines()
 
