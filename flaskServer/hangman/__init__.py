d328d580d2e910779a5ff567fee2ae9bf2cbb636
Jeffrey Arnold <jeffarnold02@gmail.com>
2024-12-04 13:14:05 -0700

    Creates Dockerfiles to make management of all of the different systems easier. Also makes slight changes to some configurations of other systems to allow for the addition of docker.

--- flaskServer/hangman/__init__.py
+++ flaskServer/hangman/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///../../sqlite database/web_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///../../sqlite_database/web_database.db'
 db = SQLAlchemy()
 db.init_app(app)
 
