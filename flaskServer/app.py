4d0c5b0ad26da21894f2e7c5b9d26a0914df3eac
JeffreyArnold24 <90009267+JeffreyArnold24@users.noreply.github.com>
2025-01-21 12:39:18 -0700

    Revert "Attempts to fix the SSL Protocol Error"

--- flaskServer/app.py
+++ flaskServer/app.py
@@ -1,15 +1,11 @@
 from flask import request
 from flask_cors import CORS
-from flask_sslify import SSLify
 from hangman import app as hangmanApp
 from authentication import app as authApp
-
 
 CORS(hangmanApp, resources={r"/*": {"origins": "http://localhost:3000"}})
 CORS(hangmanApp, resources={r"/*": {"origins": "http://jeffreyarnoldportfolio.com"}})
 
 if __name__ == '__main__':
-    sslify = SSLify(hangmanApp)
-    sslify = SSLify(authApp)
-    hangmanApp.run(debug=False, host="0.0.0.0", port=8081, ssl_context=('cert.pem', 'key.pem'))
-    authApp.run(debug=False, host="0.0.0.0", port=8082, ssl_context=('cert.pem', 'key.pem'))
+    hangmanApp.run(debug=False, host="0.0.0.0", port=8081)
+    authApp.run(debug=False, host="0.0.0.0", port=8082)
