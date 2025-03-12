a790ff717ae9f49e2942323e05ccec25c595be2a
Jeffrey Arnold <jeffarnold02@gmail.com>
2025-03-12 13:33:45 -0600

    Created the rubric for the "about me" section. More changes coming.

--- reactApplication/react_next_app/src/app/about_page/page.js
+++ reactApplication/react_next_app/src/app/about_page/page.js
@@ -3,17 +3,28 @@
 import Toolbar from "@/components/toolbar/top_toolbar";
 import { componentDescriptions } from "@/constants/descriptions_constants";
 import Accordion from "@/components/accordion/accordion";
+import styles from "./about.css"
+import Image from "next/image";
 
 export default function About() {
 
     const selfDescription = componentDescriptions.aboutMe
-  const signInDescription = componentDescriptions.signIn
+    const signInDescription = componentDescriptions.signIn
     
     return (
-    <div className="games-container">
+    <div className="about-container">
         <div><Toolbar /></div>
         {/* Description and Sign-In Section */}
-        <div className="welcome-secondary-section">
+        <div className="about-me-section">
+            <div className="about-me-image">
+                <Image src="/about_me/profile_pic.jpg" alt="Profile" width={308} height={412} className="profileImage" />;
+            </div>
+            <div className="about-me-info">
+                <h1>Jeffrey Arnold</h1>
+                <p>Short bio about yourself. Mention your background, interests, and skills.</p>
+                <p>Email: jeffreyarnold@gmail.com</p>
+            </div>
+        </div>
             {/* Description Section on the left */}
             <div className="welcome-description-section">
             <div className="welcome-main_info-section">
@@ -26,7 +37,7 @@ export default function About() {
             </div>
 
             
-        </div>
+        
         <div className = "sign-in-description">
             <Accordion title = "Sign In" description={signInDescription}/>
         </div>
