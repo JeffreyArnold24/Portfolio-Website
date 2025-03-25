d39b71f61b41d53567b98e59f9fc5ec4507bbca2
Jeffrey Arnold <jeffarnold02@gmail.com>
2025-03-25 15:47:48 -0600

    Fixes a bug in the login service were a user would be given a sign in error if they logged on using one browser and then tried it again in a different browser.

--- springBootServer/src/main/java/com/DAOs/AuthTokenDAOInterface.java
+++ springBootServer/src/main/java/com/DAOs/AuthTokenDAOInterface.java
@@ -8,5 +8,7 @@ public interface AuthTokenDAOInterface {
     public Boolean deleteAuthToken(String authToken);
     public Boolean deleteExpiredTokens();
     public Boolean createAuthToken(String username, String authToken, LocalDateTime creationDateTime);
+    public Boolean tokenExistsForUsername(String username);
+    public String retreiveAuthToken(String username);
 
 }
