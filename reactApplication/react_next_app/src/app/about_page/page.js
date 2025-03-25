f2f6e52d91a085bebffda1dacfbc0c6a058d868f
Jeffrey Arnold <jeffarnold02@gmail.com>
2025-03-25 09:21:42 -0600

    Finalizes the about page. This includes adding descriptions and adding css to make it look good.

--- reactApplication/react_next_app/src/app/about_page/page.js
+++ reactApplication/react_next_app/src/app/about_page/page.js
@@ -5,10 +5,12 @@ import { componentDescriptions } from "@/constants/descriptions_constants";
 import Accordion from "@/components/accordion/accordion";
 import styles from "./about.css"
 import Image from "next/image";
+import React from "react";
 
 export default function About() {
 
     const selfDescription = componentDescriptions.aboutMe
+    const siteDescription = componentDescriptions.aboutSite
     const signInDescription = componentDescriptions.signIn
     
     return (
@@ -17,31 +19,18 @@ export default function About() {
         {/* Description and Sign-In Section */}
         <div className="about-me-section">
             <div className="about-me-image">
-                <Image src="/about_me/profile_pic.jpg" alt="Profile" width={308} height={412} className="profileImage" />;
+                <Image src="/about_me/profile_pic.jpg" alt="Profile" width={308} height={412} className="profileImage" />
             </div>
             <div className="about-me-info">
                 <h1>Jeffrey Arnold</h1>
-                <p>Short bio about yourself. Mention your background, interests, and skills.</p>
-                <p>Email: jeffreyarnold@gmail.com</p>
+                <p>{selfDescription}</p>
+                <p>Email: jeffarnold@gmail.com</p>
             </div>
         </div>
-            {/* Description Section on the left */}
-            <div className="welcome-description-section">
-            <div className="welcome-main_info-section">
-                <div>{selfDescription.split('\n').map((line, index) => (
-                    <div key={index}>
-                        {line}
-                    </div>
-                    ))}</div>
-            </div>
-            </div>
-
-            
-        
-        <div className = "sign-in-description">
-            <Accordion title = "Sign In" description={signInDescription}/>
-        </div>
-        
+        <div className= "about-site-section">
+            <h1>About the Site</h1>
+            <p>{siteDescription}</p>
+        </div>        
     </div>
     );
 }
